b13773da63cb4afa6e7f203f57b09f8d38d6bb93
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-29 21:54:05 +0530

    Harmonic Number push

---
+++ Harmonic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Harmonic
+    {
+        public static void HarmonicNumber()
+        {
+            Console.WriteLine("Harmonic Number");
+
+            Console.WriteLine("Enter number : ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0;
+
+            for (double count = 1; count <= num; count++)
+            {
+                Console.Write("1/{0} + ", count);
+                sum = sum + 1 / count;
+            }
+
+
+            Console.WriteLine("\nThe sum of {0}  series is {1}  ", num, sum);
+            Console.ReadLine();
+        }
+    }
+}
