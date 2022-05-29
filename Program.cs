fa8db0e68371383d19dff9cca252495fd3d5616b
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-29 21:17:43 +0530

    program.cs push

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        /// <summary>
+        /// Basic Core programs day5
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[]args)
+        {
+            //VowelOrConstant.CheckVowelOrConstant();
+            //SwapTwoNumbers.SwappingNumbers();
+            //Division.CalculateQuotientAndRemainder();
+            // EvenOrOdd.CheckEvenOrOdd();
+            //LargestNumber.FindingLargestNumber();
+            // FlipCoin.FlipTheCoin();
+            //LeapYear.TheLeapYear();
+            //PowerOfTwo.ThePowerOfTwo();
+            //Harmonic.HarmonicNumber();
+            Factors.PrimeFactors();
+        }
+    }
+}
