f0ff54d17b7747b8abe9aafc3deb66f7fb6ae152
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-29 21:36:09 +0530

    leapyear push

---
+++ LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class LeapYear
+    {
+        public static void TheLeapYear()
+        {
+            Console.WriteLine("Enter year");
+
+            int year = Convert.ToInt32(Console.ReadLine());   //user input
+
+
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))     //checking for leap year or not
+            {
+                Console.WriteLine("{0} is a leap year  ", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not leap year  ", year);
+            }
+        }
+    }
+}
