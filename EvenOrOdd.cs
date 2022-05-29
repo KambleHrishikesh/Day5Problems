f4d1c130389370b82dd41e9176a77f2338edba32
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-29 22:34:20 +0530

    even or odd push

---
+++ EvenOrOdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class EvenOrOdd
+    {
+        public static void CheckEvenOrOdd()
+        {
+            int num;
+            Console.WriteLine("enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            if (num%2 ==0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+        }
+    }
+}
